0a0a5d45823d4299c217115660538a1ace014b0f
Anna Zhelnina <amie82@list.ru>
2023-02-19 11:22:15 +0700

    dom3.2

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,20 +1,15 @@
 const redBox = document.querySelector('.chat-widget');
-redBox.onclick = () => redBox.classList.add('chat-widget_active');  
-const inputField = document.querySelector('.chat-widget__input');
-inputField.addEventListener('keypress', function(event){
-    if (event.key === 'Enter' && inputField.value) {
-        let timeStamp = new Date();
-        messages.innerHTML += `
-            <div class="message message_client">
-                <div class="message__time">
-                    ${addZero(timeStamp.getHours())}:${addZero(timeStamp.getMinutes())}
-                </div>
-                <div class="message__text">
-                    ${inputField.value}
-                </div>
-            </div>
-        `;
-        inputField.value = null; 
-        setTimeout(autoAnswer, 500);
+const inputElement = document.getElementById('chat-widget__input');
+const chatContainer = document.querySelector('.chat-widget__messages');
+redBox.addEventListener('click', () => {
+    if (!Array.from(redBox).includes('chat-widget_active')) {
+      redBox.classList.add('chat-widget_active');
     }
-});
+  });
+
+
+
+
+
+
+
